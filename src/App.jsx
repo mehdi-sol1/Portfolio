21b635dbb1059146464e0d16dfe9baae6b10c996
mehdi-sol1 <113058397+mehdi-sol1@users.noreply.github.com>
2023-12-10 21:36:55 +0100

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import Header from './components/Header'
-import Body from './components/body'
+import Body from './components/Body'
 import './index.css'
 import Skills from './components/Skills'
 import About from './components/About'
